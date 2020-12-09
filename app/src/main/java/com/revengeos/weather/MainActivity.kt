30ecf452c8c658d0776a40e0be67350381532a4d
Lucchetto <lucchetto.tie@live.com>
2020-12-09 21:14:25 +0100

    Convert MainActivity to Kotlin
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

---
+++ app/src/main/java/com/revengeos/weather/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.revengeos.weather
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var feedFragment: FeedFragment
+    private lateinit var citiesFragment: CitiesFragment
+    private lateinit var settingsFragment: SettingsFragment
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val mMainNav = findViewById<BottomNavigationView>(R.id.main_nav)
+        feedFragment = FeedFragment()
+        citiesFragment = CitiesFragment()
+        settingsFragment = SettingsFragment()
+        setFragment(feedFragment)
+        mMainNav.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_feed -> {
+                    setFragment(feedFragment)
+                    true
+                }
+                R.id.nav_cities -> {
+                    setFragment(citiesFragment)
+                    true
+                }
+                R.id.nav_settings -> {
+                    setFragment(settingsFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.main_frame, fragment)
+        fragmentTransaction.commit()
+    }
+}
