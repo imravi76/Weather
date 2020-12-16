d39f017757fd1a2c0f67d9fbdfe1f046f15af175
Lucchetto <lucchetto.tie@live.com>
2020-12-16 18:35:50 +0100

    Update extra weather data UI [2/2]
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- app/src/main/java/com/revengeos/weather/WeatherDataGridView.kt
+++ app/src/main/java/com/revengeos/weather/WeatherDataGridView.kt
@@ -7,6 +7,7 @@ import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TableLayout
 import androidx.annotation.Nullable
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.flexbox.FlexWrap
 import com.google.android.flexbox.FlexboxLayout
 import com.revengeos.revengeui.view.ExpandableIndicator
@@ -14,14 +15,21 @@ import com.revengeos.weather.util.WeatherUtils.Companion.getFormattedDistance
 import com.revengeos.weather.util.WeatherUtils.Companion.getFormattedSpeed
 import com.revengeos.weather.util.WeatherUtils.Companion.getFormattedTemperature
 import com.revengeos.weather.util.WeatherUtils.Companion.getTimeFromEpoch
+import rjsv.expframelayout.ExpandableFrameLayout
 
-class WeatherDataGridView : LinearLayout {
+class WeatherDataGridView : ConstraintLayout {
 
+    private val sunriseView: IconTextView
+    private val sunsetView: IconTextView
+    private val pressureView: IconTextView
     private val humidityView: IconTextView
     private val windView: IconTextView
+    private val visibilityDistanceView : IconTextView
     private val minTempView: IconTextView
     private val maxTempView: IconTextView
+
     private val expandableIndicator : ExpandableIndicator
+    private val additionalData : ExpandableFrameLayout
 
     constructor (context: Context) : this(context, null) {
     }
@@ -35,14 +43,23 @@ class WeatherDataGridView : LinearLayout {
     constructor(context: Context, @Nullable attrs: AttributeSet?, defStyleAttr: Int, defStyleRes : Int) : super(context, attrs, defStyleAttr, defStyleRes) {
         LayoutInflater.from(context).inflate(R.layout.weather_data_grid, this, true);
 
+        sunriseView = findViewById(R.id.current_sunrise)
+        sunsetView = findViewById(R.id.current_sunset)
+        pressureView = findViewById(R.id.current_pressure)
         humidityView = findViewById(R.id.current_humidity)
         windView = findViewById(R.id.current_wind)
+        visibilityDistanceView = findViewById(R.id.current_visibility)
         minTempView = findViewById(R.id.current_min_temp)
         maxTempView = findViewById(R.id.current_max_temp)
+
+        additionalData = findViewById(R.id.additional_data)
+        additionalData.collapse()
+        additionalData.animationDuration = 700
 
         expandableIndicator = findViewById(R.id.expandable_indicator)
         expandableIndicator.setOnClickListener { view ->
             expandableIndicator.toggleExpansion()
+            additionalData.toggle()
         }
     }
 
@@ -52,9 +69,13 @@ class WeatherDataGridView : LinearLayout {
         minTempView.textView.text = getFormattedTemperature(minTemp)
         maxTempView.textView.text = getFormattedTemperature(maxTemp)
 
+        sunriseView.textView.text = getTimeFromEpoch(sunrise, timeZone)
+        sunsetView.textView.text = getTimeFromEpoch(sunset, timeZone)
+        pressureView.textView.text = "$pressure hPa"
         humidityView.textView.text = "$humidity %"
         windView.iconView.rotation = windDirection.toFloat()
         windView.textView.text = getFormattedSpeed(windSpeed)
+        visibilityDistanceView.textView.text = getFormattedDistance(visibility.toFloat())
 
     }
 }
